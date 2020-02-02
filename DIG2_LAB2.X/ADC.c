52d330fa7851d96749180f76626e4cd3649e82ca
JosueAsturias <45212763+JosueAsturias@users.noreply.github.com>
2020-02-01 22:47:22 -0600

    Botones
    Interrupcion de botones funciona

---
+++ DIG2_LAB2.X/ADC.c
@@ -0,0 +1,11 @@
+/*
+ * File:   ADC.c
+ * Author: josue
+ *
+ * Created on January 30, 2020, 4:06 PM
+ */
+
+
+#include <xc.h>
+
+
--- DIG2_LAB2.X/main.c
+++ DIG2_LAB2.X/main.c
@@ -31,12 +31,56 @@
 
 #include <xc.h>
 #include <stdint.h>
+#define _XTAL_FREQ 4000000
 
 void config_PUERTOS(void);
+uint8_t  banderaBoton = 0;
+
+
+
+void __interrupt() ISR(void){
+    if (INTCONbits.RBIF == 1){             // IOCB
+        if (banderaBoton == 0){
+            if (PORTBbits.RB0 == 0){
+                banderaBoton = 1;
+                INTCONbits.RBIE = 0;
+            }
+            else if (PORTBbits.RB2 == 0){
+                banderaBoton = 2;
+                INTCONbits.RBIE = 0;
+            }
+        }
+        INTCONbits.RBIF = 0;
+    }
+    
+    
+      return;  
+    }
 
 void main(void) {
     config_PUERTOS();
+    INTCONbits.GIE = 1;
+    INTCONbits.RBIF = 0;
+    while(1){
+        switch (banderaBoton){
+            case 1:
+                __delay_ms(150);
+                PORTA = PORTA + 1;
+                banderaBoton = 0;
+                INTCONbits.RBIE = 1;
+                
+                break;
+            case 2:
+                __delay_ms(150);
+                PORTA = PORTA - 1;
+                banderaBoton = 0;
+                INTCONbits.RBIE = 1;
+                break;
+            default:
+                PORTA = PORTA;
+            }
 
+        }
     return;
 }
 
@@ -45,14 +89,18 @@ void config_PUERTOS(void){
     TRISD = 0;
     TRISC = 0;
     TRISA = 0;
-    TRISB = 0b00000111;
-    PORTA = 0; PORTB = 0; PORTC = 0; PORTD = 0;
+    TRISB = 0b00000101;
+    PORTA = 0; 
+    PORTB = 0; 
+    PORTC = 0; 
+    PORTD = 0;
     ANSEL = 0;
     ANSELH = 0; 
     WPUB = 0b00000101;
     OPTION_REGbits.nRBPU = 0;
     
     //Interrupciones
-    IOCB = 0b00000101;  //RB0 y RB2 tiene interrupcion
-    INTCONbits.GIE = 1;
+    IOCB = 0b00000101;;  //RB0 y RB2 tiene interrupcion
+    INTCONbits.RBIE = 1;
+    return;
 }
